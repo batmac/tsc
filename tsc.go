118a74343747edfe763404006ebdc0f94667f715
Baptiste Canton <bat@sbz.fr>
2019-09-11 00:42:05 +0200

    move calls to UnixNano() in Frequency()

--- tsc.go
+++ tsc.go
@@ -10,16 +10,16 @@ func Rdtscp() (uint64, uint32)
 
 // will take n*period to find out
 func Frequency(n int, period time.Duration) float64 {
-	var startTime, endTime int64
+	var startTime, endTime time.Time
 	var startCounter, endCounter uint64
 	freq := 9.9e9
 	for i := 0; i < n; i++ {
-		startTime = time.Now().UnixNano()
+		startTime = time.Now()
 		startCounter = BenchmarkStart()
 		time.Sleep(period)
 		endCounter = BenchmarkEnd()
-		endTime = time.Now().UnixNano()
-		elapsed := float64(endTime-startTime) / 1000000000
+		endTime = time.Now()
+		elapsed := float64(endTime.UnixNano()-startTime.UnixNano()) / 1000000000
 		iterfreq := float64(endCounter-startCounter) / elapsed
 		if iterfreq < freq {
 			freq = iterfreq
